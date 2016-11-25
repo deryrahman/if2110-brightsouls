98df71001169a71f7bc544413496d3a1a36c8153
mita <rizky.faramitha@gmail.com>
2016-11-25 10:13:44 +0700

    nambah instruction di splashscreen

--- src/menus/endgame.c
+++ src/menus/endgame.c
@@ -25,18 +25,18 @@ void EndGameShow (GameState* gameState) {
 
 	 	String str = StringCreate("Your HP : ");
 	 	StringAppendString(&str,StringFromUint(player->HP));
-	 	UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 14, PixelStyleCreateDefault(), str);
+	 	UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 14, PixelStyleCreate(RESET,BLACK, GREEN), str);
 		
 		str = StringCreate("Your Defense : ");
 		StringAppendString(&str,StringFromUint(player->DEF));
-	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 16, PixelStyleCreateDefault(), str);
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 16, PixelStyleCreate(RESET,BLACK, BLUE), str);
 
 	    str = StringCreate("Your Strength : ");
 	    StringAppendString(&str,StringFromUint(player->STR));
-	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 18, PixelStyleCreateDefault(), str);
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 18, PixelStyleCreate(RESET,BLACK, YELLOW), str);
 
 	    str = StringCreate("We heard that you are a great player, so let's fight again!");
-	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 18, PixelStyleCreateDefault(), str);
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 20, PixelStyleCreateDefault(RESET,BLACK, GREEN), str);
     }
     else {
 	    /* Membaca file yang berisi text untuk kasus kalah */
@@ -49,13 +49,16 @@ void EndGameShow (GameState* gameState) {
 	    }
 
 	    String str = StringCreate("We are so sorry that you are just bitten by our boss");
-	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 18, PixelStyleCreateDefault(), str);
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 14, PixelStyleCreate(RESET,BLACK,GREEN), str);
 
-	    str = StringCreate("But of course you can do the reincarnation to play again!");
-	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 16, PixelStyleCreateDefault(), str);
+	    str = StringCreate("We wish you have an even brighter soul");
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 16, PixelStyleCreate(RESET,BLACK, BLUE), str);
+
+	    str = StringCreate("And of course you can do the reincarnation to play again!");
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 18, PixelStyleCreate(RESET,BLACK, YELLOW), str);
 
 	    str = StringCreate("See you & better luck next time!");
-	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 18, PixelStyleCreateDefault(), str);
+	    UIDrawText(*terminal,TerminalGetCenterX(*terminal, StringLength(str)), ImageHeight(mainmenuImage) + 20, PixelStyleCreate(RESET,BLACK, GREEN), str);
     }
 
     TerminalRender(*terminal);
--- src/menus/splashscreen.c
+++ src/menus/splashscreen.c
@@ -1,6 +1,14 @@
-#include "graphics/terminal.h"
-#include "graphics/ui.h"
+#include "player.h"
+#include "xstring.h"
+#include <stdio.h>
 #include "menus/splashscreen.h"
+#include "graphics/terminal.h"
+#include "graphics/pixel.h"
+#include "graphics/ui.h"
+#include "xstring.h"
+#include "menus/mainmenu.h"
+#include "menus/newgame.h"
+#include "gamestate.h"
 
 void SplashScreenShow(GameState* gameState) {
     Terminal *terminal = gameState->terminal;
@@ -16,4 +24,46 @@ void SplashScreenShow(GameState* gameState) {
     TerminalRender(*terminal);
 
     String str = StringCreate(""); StringReadln(&str);
+
+    Terminal *terminalIns = gameState->terminal;
+    TerminalClear(*terminalIns);
+    UIDrawBoxLine(*terminalIns, 1, 1, TerminalGetWidth(*terminalIns) - 2, TerminalGetHeight(*terminalIns) - 2, PixelStyleCreateDefault(), MULTILINE);
+
+    FILE *fileI = fopen("res/instruction.img","r");
+    Image mainmenuImage;
+    if(fileI) {
+        mainmenuImage = ImageCreateFromFile(fileI, PixelStyleCreateDefault());
+        UIDrawImage(*terminal, 4, 3, mainmenuImage);
+        fclose(fileI);
+    }
+    str = StringCreate("Please do remember that every command that you type ends with enter.");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 6, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("In the menu screen you can choose to:");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 8, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("1. New Game, to select this please type 'New Game'");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 10, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("2. Start Game, to select this please type 'Start Game'");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 12, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("3. Load, to select this please type 'Load'");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 14, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("4. Exit, to select this please type 'Exit'");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 16, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("While you are looking for you enemy or medicine, you can press the up, down, left, or right button of the keyboard to move.");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 18, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("When you are in the battle mode, you can type 'A' to attack, 'B' to defense, or 'F' to flank.");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 20, PixelStyleCreateDefault(), str);
+
+    str = StringCreate("Press enter to continue...");
+    UIDrawText(*terminal,4, ImageHeight(mainmenuImage) + 22, PixelStyleCreateDefault(), str);
+
+    TerminalRender(*terminal);
+    str = StringCreate("");
+    StringReadln(&str);
 }
