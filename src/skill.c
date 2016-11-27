9ef80a6cefd05085d754d3493840dd439143b410
yesasurya <rahmadyesas@gmail.com>
2016-11-27 22:37:18 +0700

    Ubah tampilan skill

--- src/skill.c
+++ src/skill.c
@@ -10,15 +10,16 @@ File	: skill.h
 #include "tree.h"
 #include "skill.h" // masih dijalankan terpisah dari main utama
 
-Skill SkillCreate(uint X, uint Y, uint exp_req, uint urutan){
+Skill SkillCreate(uint X, uint Y, uint exp_req, uint urutan, String nama){
 	Skill P;
 	Attack(P)=X;
 	Deffense(P)=Y;
 	EXP(P)=exp_req;
 	Urutan(P)=urutan;
+	Nama(P)=nama;
 	return P;
 }
 
-void SkillMake(Skill *skill, uint X, uint Y, uint exp_req, uint urutan){
-	*skill=SkillCreate(X,Y,exp_req,urutan);
+void SkillMake(Skill *skill, uint X, uint Y, uint exp_req, uint urutan, String nama){
+	*skill=SkillCreate(X,Y,exp_req,urutan,nama);
 }
